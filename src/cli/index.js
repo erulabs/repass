287409ced01fb588e57c1d059ad35882002f5be2
Seandon Mooy <seandon.mooy@gmail.com>
2016-01-20 14:20:09 -0800

    cli

--- src/cli/index.js
+++ src/cli/index.js
@@ -10,32 +10,26 @@ import path from 'path'
 
 const argv = yargs
   .usage(USAGE)
-  // Common options:
-  .help('h')
-  .alias('h', 'help')
+  .alias('c', 'client-id')
+  .describe('c', 'Yubico Client_ID')
+  .demand('c')
+  .alias('k', 'secret-key')
+  .describe('k', 'Yubico Secret Key')
+  .demand('k')
   // Options:
-  .default('k', '~/.ssh/id_rsa')
-  .alias('k', 'key')
-  .describe('k', 'SSH key file')
+  .describe('i', 'SSH key file')
+  .default('i', '~/.ssh/id_rsa')
+  .alias('i', 'identity-file')
+  // Optional S3 creds
+  .describe('iam', 'AWS IAM ID')
+  .describe('secret', 'AWS IAM Secret')
   // Commands:
-  // GET
   .command('get', 'get the password for a site')
-  .example(`${cliName} get somesite.com`)
-  // SET
   .command('set', 'set the password for a site')
-  .example(`${cliName} set somesite.com`)
-  // DEL
   .command('del', 'delete an entry')
-  .example(`${cliName} del somesite.com`)
-  // LS
   .command('ls', 'list entries')
-  .example(`${cliName} ls`)
-  // Generate
   .command('gen', 'Create a new password (without storing it)')
-  .example(`${cliName} ls`)
-  // Rekey
   .command('regen', 'Regenerate passwords')
-  .example(`${cliName} regen [somesite.com]`)
   .demand(1)
   .argv
 
@@ -56,8 +50,12 @@ inquirer.prompt([
 ], function (result) {
   const repass = new Repass({
     otp: result.otp,
-    key: path.normalize(argv.key.replace('~', getUserHome())),
-    passphrase: result.otp
+    key: path.normalize(argv.i.replace('~', getUserHome())),
+    passphrase: result.otp,
+    client_id: argv.c,
+    secret_key: argv.k,
+    iam: argv.iam,
+    secret: argv.secret
   })
   const doAction = function (action, args) {
     repass[action](...args)
--- src/lib/Repass.js
+++ src/lib/Repass.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import Random from 'random-js'
+
 const RepassOptionsError = function (msg) {
   this.name = 'RepassOptionsError'
   this.message = msg || 'Repass Options incomplete'
@@ -26,5 +28,9 @@ export class Repass {
   }
   regen () {
 
+  }
+  gen (seed) {
+    const random = new Random(Random.engines.mt19937().autoSeed())
+    console.log(random.string())
   }
 }
