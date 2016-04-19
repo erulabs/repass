d02ad1a8a79a7e0a6b7482da76a1b505e4b95d87
erulabs <seandon.mooy@gmail.com>
2016-04-19 02:49:01 -0700

    last fix for green

--- test/cli.sh
+++ test/cli.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEST="At least runs and prints usage"
-if node ./_build/cli/index.js 2>&1 | fgrep Usage &> /dev/null; then
+if node ./build/cli/index.js 2>&1 | fgrep Usage &> /dev/null; then
   echo "${TEST}: OK"
 else
   echo "${TEST}: failed"
